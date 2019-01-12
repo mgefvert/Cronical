5f1dd83e123b1767f89762fed30e1356fe82ab6a
Mats Gefvert <mats@gefvert.org>
2019-01-12 12:46:17 -0500

    New build

--- Cronical/Properties/AssemblyInfo.cs
+++ Cronical/Properties/AssemblyInfo.cs
@@ -26,6 +26,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyVersion("1.3.*")]
 
 [assembly: InternalsVisibleTo("Cronical.Test")]
